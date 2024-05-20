7ab90812193c4b19d808b5837d9b4fdb5914b30d
Clueless7 <catalanjosel123@gmail.com>
2024-05-20 10:08:55 +0800

    task: change morgan format depending on NODE_ENV

--- backend/src/middlewares/logger.middleware.ts
+++ backend/src/middlewares/logger.middleware.ts
@@ -1,8 +1,10 @@
 import morgan, { type StreamOptions } from 'morgan'
 import { logger } from '../utils/logger.util.js'
 
+const morganFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
+
 const stream: StreamOptions = {
   write: (message) => logger.http(message),
 }
 
-export const loggerMiddleware = morgan('common', { stream })
+export const loggerMiddleware = morgan(morganFormat, { stream })
