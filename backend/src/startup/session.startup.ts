074080a678e787258300497a1171828ebca6fe0a
Clueless7 <catalanjosel123@gmail.com>
2024-05-20 14:06:36 +0800

    task: set maxAge option to 9 hours in session cookie

--- backend/src/startup/session.startup.ts
+++ backend/src/startup/session.startup.ts
@@ -2,10 +2,20 @@ import type { Express } from 'express'
 import session from 'express-session'
 import { config } from '../config/env.config.js'
 
+const maxAge = 1000 * 60 * 60 * 9 // 9 Hours
+
 const options: session.SessionOptions = {
   secret: config.API_SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    sameSite: 'lax', // The best setting would be 'strict' but that requires the frontend to have the same domain as the backend(this api)
+    // lax only sends cookie to GET, and HEAD requests to different origin
+    // none always sends cookie to a different origin but needs sameSite: true
+    secure: false, // This depends if the domain this server will be deployed have https
+    maxAge,
+  },
 }
 
 export const initSession = (app: Express) => {
