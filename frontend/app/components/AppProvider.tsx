7f8e6b61d77f8265ae0da40cd24368a979018d56
Clueless7 <catalanjosel123@gmail.com>
2024-05-21 16:31:49 +0800

    task: change default query config

--- frontend/app/components/AppProvider.tsx
+++ frontend/app/components/AppProvider.tsx
@@ -1,13 +1,17 @@
 'use client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { DefaultOptions, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
+export const queryConfig = {
+  queries: {
+    refetchOnWindowFocus: false,
+    retry: false,
+    staleTime: 1000 * 60,
   },
+} satisfies DefaultOptions
+
+const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
 })
 
 export default function AppProvider({ children }: { children: React.ReactNode }) {
