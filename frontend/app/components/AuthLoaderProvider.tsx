9e3dbda63da58cd716adba84b65f5179d3b824ca
Clueless7 <catalanjosel123@gmail.com>
2024-05-21 11:14:47 +0800

    task: show login page when user is unauthenticated

--- frontend/app/components/AuthLoaderProvider.tsx
+++ frontend/app/components/AuthLoaderProvider.tsx
@@ -1,5 +1,13 @@
 import { AuthLoader } from '../lib/auth'
+import Login from '../login/page'
 
 export default function AuthLoaderProvider({ children }: { children: React.ReactNode }) {
-  return <AuthLoader renderLoading={() => <div>AuthLoader loading...</div>}>{children}</AuthLoader>
+  return (
+    <AuthLoader
+      renderLoading={() => <div>AuthLoader loading...</div>}
+      renderUnauthenticated={() => <Login />}
+    >
+      {children}
+    </AuthLoader>
+  )
 }
