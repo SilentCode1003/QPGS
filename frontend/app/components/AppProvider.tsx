834e7db3f1e21f4783e629024b774f38af4a95b4
Clueless7 <catalanjosel123@gmail.com>
2024-05-21 09:27:20 +0800

    task: do not refetch on window focus react query

--- frontend/app/components/AppProvider.tsx
+++ frontend/app/components/AppProvider.tsx
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthLoader } from '../lib/auth'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function AppProvider({ children }: { children: React.ReactNode }) {
   return (
