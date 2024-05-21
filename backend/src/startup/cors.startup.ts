2a2770a402200a11c5c18254579555ba58068293
Clueless7 <catalanjosel123@gmail.com>
2024-05-21 11:43:38 +0800

    refactor: make cors change depending on node env

--- backend/src/startup/cors.startup.ts
+++ backend/src/startup/cors.startup.ts
@@ -3,23 +3,20 @@ import type { Express } from 'express'
 import { config } from '../config/env.config'
 import { logger } from '../utils/logger.util'
 
-const frontendHostPort = `${config.FRONTEND_HOST}:${config.FRONTEND_PORT}`
+const frontendUrl = `${config.FRONTEND_HOST}:${config.FRONTEND_PORT}`
+
+// Only allow these clients
+// This is mainly what url the frontend in the browser sees
+// You can also add the ip of another server
+const origin = config.NODE_ENV === 'production' ? [frontendUrl] : ['http://localhost:3001'] // url of next application
 
 const options: cors.CorsOptions = {
-  // Only allow these clients
-  // This is mainly what url the frontend in the browser sees
-  // You can also add the ip of another server
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:3001',
-    frontendHostPort,
-  ],
+  origin,
   // Set to true if you want the client to send cookies
   credentials: true,
 }
 
 export const initCors = (app: Express) => {
-  logger.info(`Adding frontend url: ${frontendHostPort} to allowed origins`)
+  logger.info(`Adding frontend url: ${frontendUrl} to allowed origins`)
   app.use(cors(options))
 }
