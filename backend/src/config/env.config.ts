088571f4e825d44c21688e9c8a83ad716bbf6192
Clueless7 <catalanjosel123@gmail.com>
2024-05-21 11:34:58 +0800

    task: set front end url as a env in backend

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -5,6 +5,8 @@ const envSchema = z.object({
   NODE_ENV: z.string(),
   API_PORT: z.coerce.number(),
   API_SESSION_SECRET: z.string(),
+  FRONTEND_HOST: z.string(),
+  FRONTEND_PORT: z.coerce.number(),
   DATABASE_URL: z.string(),
 })
 
@@ -19,5 +21,7 @@ export const config = {
   NODE_ENV: validatedEnv.data.NODE_ENV,
   API_PORT: validatedEnv.data.API_PORT,
   API_SESSION_SECRET: validatedEnv.data.API_SESSION_SECRET,
+  FRONTEND_HOST: validatedEnv.data.FRONTEND_HOST,
+  FRONTEND_PORT: validatedEnv.data.FRONTEND_PORT,
   DATABASE_URL: validatedEnv.data.DATABASE_URL,
 }
--- backend/src/startup/cors.startup.ts
+++ backend/src/startup/cors.startup.ts
@@ -1,15 +1,25 @@
-import type { Express } from 'express'
 import cors from 'cors'
+import type { Express } from 'express'
+import { config } from '../config/env.config'
+import { logger } from '../utils/logger.util'
+
+const frontendHostPort = `${config.FRONTEND_HOST}:${config.FRONTEND_PORT}`
 
 const options: cors.CorsOptions = {
   // Only allow these clients
   // This is mainly what url the frontend in the browser sees
   // You can also add the ip of another server
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:3001'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:3001',
+    frontendHostPort,
+  ],
   // Set to true if you want the client to send cookies
   credentials: true,
 }
 
 export const initCors = (app: Express) => {
+  logger.info(`Adding frontend url: ${frontendHostPort} to allowed origins`)
   app.use(cors(options))
 }
