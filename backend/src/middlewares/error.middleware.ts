0ee6a72d2f0b3f75c78bee33df2cebb7e577d907
Clueless7 <catalanjosel123@gmail.com>
2024-05-25 14:05:50 +0800

    task: default to err.message instead of err

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -27,6 +27,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   }
 
   // If we didn't handle any of the error types above
-  logger.error(JSON.stringify(err))
-  res.status(500).json({ message: err })
+  logger.error(JSON.stringify(err.message ? err.message : err))
+  res.status(500).json({ message: err.message ? err.message : err })
 }
