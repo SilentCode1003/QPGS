e08a9314183110605e20a4429ae52d5afaba22c2
Clueless7 <catalanjosel123@gmail.com>
2024-05-29 09:03:07 +0800

    task: create client routes and handlers

---
+++ backend/src/routes/client.route.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import {
+  createClient,
+  deleteClient,
+  getClient,
+  getClients,
+  updateClient,
+} from '../handlers/client.handler'
+import { isLoggedIn } from '../middlewares/auth.middleware'
+import { isAdmin } from '../middlewares/role.middleware'
+
+export const clientRouter = express.Router()
+
+clientRouter.get('/', isLoggedIn, getClients)
+clientRouter.post('/', isLoggedIn, getClient)
+clientRouter.get('/:id', isLoggedIn, createClient)
+clientRouter.put('/:id', isLoggedIn, isAdmin, updateClient)
+clientRouter.delete('/:id', isLoggedIn, isAdmin, deleteClient)
--- backend/src/startup/route.startup.ts
+++ backend/src/startup/route.startup.ts
@@ -2,10 +2,12 @@ import type { Express } from 'express'
 import { authRouter } from '../routes/auth.route.js'
 import { paymentTypeRouter } from '../routes/payment-type.route.js'
 import { userRouter } from '../routes/user.route.js'
+import { clientRouter } from '../routes/client.route.js'
 
 // Init routes adds all routes to app
 export const initRoutes = (app: Express) => {
   app.use('/auth', authRouter)
   app.use('/users', userRouter)
   app.use('/payment-types', paymentTypeRouter)
+  app.use('/clients', clientRouter)
 }
