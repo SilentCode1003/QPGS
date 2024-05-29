5fc9645d6601a889a494ead7723f64d618195a11
Clueless7 <catalanjosel123@gmail.com>
2024-05-29 13:17:43 +0800

    task: create terms and conditions routes and handlers

--- backend/src/startup/route.startup.ts
+++ backend/src/startup/route.startup.ts
@@ -1,8 +1,9 @@
 import type { Express } from 'express'
 import { authRouter } from '../routes/auth.route.js'
-import { paymentTypeRouter } from '../routes/payment-type.route.js'
-import { userRouter } from '../routes/user.route.js'
 import { clientRouter } from '../routes/client.route.js'
+import { paymentTypeRouter } from '../routes/payment-type.route.js'
+import { termsAndConditionRouter } from '../routes/terms.route.js'
+import { userRouter } from '../routes/user.route.js'
 
 // Init routes adds all routes to app
 export const initRoutes = (app: Express) => {
@@ -10,4 +11,5 @@ export const initRoutes = (app: Express) => {
   app.use('/users', userRouter)
   app.use('/payment-types', paymentTypeRouter)
   app.use('/clients', clientRouter)
+  app.use('/terms-and-conditions', termsAndConditionRouter)
 }
