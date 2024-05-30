f9f4c5dd99a55b9a819616e987877e838da7a7c9
Clueless7 <catalanjosel123@gmail.com>
2024-05-30 14:41:41 +0800

    task: make error logger log all of properties

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -28,6 +28,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
   // If we didn't handle any of the error types above
   // logger.error(JSON.stringify(err.message ? err.message : err))
-  logger.error(JSON.stringify(err.message || err))
+  logger.error(JSON.stringify(err, Object.getOwnPropertyNames(err)))
   res.status(500).json({ message: err.message || err })
 }
