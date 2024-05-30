3457ecb9165b2624132cb2e7ed57695fcbb6c070
Clueless7 <catalanjosel123@gmail.com>
2024-05-30 14:32:31 +0800

    refactor: use or operator instead of ternary

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -27,6 +27,7 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   }
 
   // If we didn't handle any of the error types above
-  logger.error(JSON.stringify(err.message ? err.message : err))
-  res.status(500).json({ message: err.message ? err.message : err })
+  // logger.error(JSON.stringify(err.message ? err.message : err))
+  logger.error(JSON.stringify(err.message || err))
+  res.status(500).json({ message: err.message || err })
 }
