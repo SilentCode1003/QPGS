ab89e6bd09b1b418c51e425b44323ad726f65e6e
Clueless7 <catalanjosel123@gmail.com>
2024-05-30 09:29:51 +0800

    fix: include product handler in route

--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import { createProduct, deleteProduct, getProduct, getProducts } from '../handlers/product.handler'
 import { isLoggedIn } from '../middlewares/auth.middleware'
 import { isAdmin } from '../middlewares/role.middleware'
 
 export const productRouter = express.Router()
 
-productRouter.get('/', isLoggedIn)
-productRouter.post('/', isLoggedIn, isAdmin)
-productRouter.get('/:id', isLoggedIn)
-productRouter.delete('/:id', isLoggedIn, isAdmin)
+productRouter.get('/', isLoggedIn, getProducts)
+productRouter.post('/', isLoggedIn, isAdmin, createProduct)
+productRouter.get('/:id', isLoggedIn, getProduct)
+productRouter.delete('/:id', isLoggedIn, isAdmin, deleteProduct)
