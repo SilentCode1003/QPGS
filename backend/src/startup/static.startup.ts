8628a32a49591a32bc379f039d69aee4c3314f4f
Clueless7 <catalanjosel123@gmail.com>
2024-06-03 11:49:10 +0800

    chore: remove console log

--- backend/src/startup/static.startup.ts
+++ backend/src/startup/static.startup.ts
@@ -4,7 +4,6 @@ import path from 'node:path'
 import { isLoggedIn } from '../middlewares/auth.middleware'
 
 export const initStatic = (app: Express) => {
-  console.log(path.join(__dirname, '../src/reports'))
   app.use('/reports', isLoggedIn)
   app.use('/reports', express.static(path.join(__dirname, '../reports')))
 }
