f87aebe537bb6b33999b8fc531a95d5b1f4194a4
Clueless7 <catalanjosel123@gmail.com>
2024-06-04 11:11:33 +0800

    doc: add comments on user route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -3,8 +3,19 @@ import { createUser, deleteUser, getUser, getUsers, updateUser } from '../handle
 
 export const userRouter = express.Router()
 
+// Only admin can access the list of users
 userRouter.get('/', getUsers)
+
+// Only admin can create a user
 userRouter.post('/', createUser)
+
+// User can only access his/her own info
+// Admin can access anyone's info
 userRouter.get('/:id', getUser)
-userRouter.put('/:id', updateUser)
+
+// User can only update his/her own info
+// Admin can update anyone's info
+userRouter.patch('/:id', updateUser)
+
+// Only admin can delete a user
 userRouter.delete('/:id', deleteUser)
