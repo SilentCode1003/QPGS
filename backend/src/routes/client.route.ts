79e3afa44bc9757205d6045add79f5e3ee510ee1
Clueless7 <catalanjosel123@gmail.com>
2024-06-04 16:07:16 +0800

    task: add handlers in client route

--- backend/src/routes/client.route.ts
+++ backend/src/routes/client.route.ts
@@ -1,20 +1,27 @@
 import express from 'express'
+import {
+  createClient,
+  deleteClient,
+  getClient,
+  getClients,
+  updateClient,
+} from '../handlers/client.handler'
 
 export const clientRouter = express.Router()
 
 // Anyone can get clients information
-clientRouter.get('/')
+clientRouter.get('/', getClients)
 
 // Anyone can create a client
-clientRouter.post('/')
+clientRouter.post('/', createClient)
 
 // Anyone can get client info
-clientRouter.get('/:id')
+clientRouter.get('/:id', getClient)
 
 // The creator of the client can update
 // Admin can update any client
-clientRouter.put('/:id')
+clientRouter.patch('/:id', updateClient)
 
 // The creator of the client can delete his/her own entry
 // Admin can delete any client
-clientRouter.delete('/:id')
+clientRouter.delete('/:id', deleteClient)
