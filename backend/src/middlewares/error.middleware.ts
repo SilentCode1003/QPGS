4db74565c067e155363b36b64cf089a7927dd469
Clueless7 <catalanjosel123@gmail.com>
2024-06-06 10:20:30 +0800

    task: change format of error logs

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -41,6 +41,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   }
 
   // If we didn't handle any of the error types above
-  logger.error(JSON.stringify(err))
-  res.status(500).json({ message: err })
+  logger.error(JSON.stringify(err, Object.getOwnPropertyNames(err)))
+  res.status(500).json({ message: err.message || 'Server error' })
 }
