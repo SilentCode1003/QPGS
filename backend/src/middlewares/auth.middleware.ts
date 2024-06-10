005bdfd2e45ea14018b7deb1c1b019c0fad41d36
Clueless7 <catalanjosel123@gmail.com>
2024-06-10 11:04:24 +0800

    task: create naive_isCommentOwnerOrAdmin middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,4 +1,4 @@
-import type { client, quotation } from '@prisma/client'
+import type { client, quotation, quotation_comment } from '@prisma/client'
 import type { RequestHandler } from 'express'
 import { CONSTANT } from '../config/constant.config'
 import { prisma } from '../db/prisma'
@@ -132,6 +132,74 @@ export const naive_isQuotationOwnerOrAdmin: RequestHandler = async (req, res, ne
   }
 
   if (quotation?.created_by !== req.session.user.id) {
+    return res.status(403).json({ message: 'Operation not allowed' })
+  }
+
+  next()
+}
+
+export const naive_isQuotationOwnerOrAdminCommentRoute: RequestHandler = async (req, res, next) => {
+  if (!req.session.user) {
+    throw new Error(
+      `naive_isQuotationOwnerOrAdminCommentRoute middleware is used on route that doesn't use ensureLogin middleware`,
+    )
+  }
+
+  if (req.session.user.role_id === CONSTANT.DB_ADMIN_ROLE_ID) {
+    next()
+    return
+  }
+
+  let quotation: quotation | null = null
+
+  try {
+    quotation = await prisma.quotation.findUnique({
+      where: {
+        id: req.params.quotationId,
+      },
+    })
+  } catch (err) {
+    next(err)
+  }
+
+  if (quotation?.created_by !== req.session.user.id) {
+    return res.status(403).json({ message: 'Operation not allowed' })
+  }
+
+  next()
+}
+
+export const naive_isCommentOwnerOrAdmin: RequestHandler = async (req, res, next) => {
+  if (!req.session.user) {
+    throw new Error(
+      `naive_isCommentOwnerOrAdmin middleware is used on route that doesn't use ensureLogin middleware`,
+    )
+  }
+
+  if (req.session.user.role_id === CONSTANT.DB_ADMIN_ROLE_ID) {
+    next()
+    return
+  }
+
+  const validatedId = numberIdParamSchema.safeParse(req.params)
+
+  if (!validatedId.success) {
+    return res.status(400).json({ message: validatedId.error.format() })
+  }
+
+  let comment: quotation_comment | null = null
+
+  try {
+    comment = await prisma.quotation_comment.findUnique({
+      where: {
+        id: validatedId.data.id,
+      },
+    })
+  } catch (err) {
+    next(err)
+  }
+
+  if (comment?.commenter_id !== req.session.user!.id) {
     return res.status(403).json({ message: 'Operation not allowed' })
   }
 
--- backend/src/routes/comment.route.ts
+++ backend/src/routes/comment.route.ts
@@ -7,7 +7,11 @@ import {
   getComments,
   getQuotationComments,
 } from '../handlers/comment.handler'
-import { isAdmin } from '../middlewares/auth.middleware'
+import {
+  isAdmin,
+  naive_isCommentOwnerOrAdmin,
+  naive_isQuotationOwnerOrAdminCommentRoute,
+} from '../middlewares/auth.middleware'
 import { quotationRouter } from './quotation.route'
 
 export const commentRouter = express.Router()
@@ -17,20 +21,24 @@ commentRouter.get('/', isAdmin, getComments)
 
 // Users can see comments on their own quotations
 // Admin can see comments on quotations
-quotationRouter.get('/:quotationId/comments', getQuotationComments)
+quotationRouter.get(
+  '/:quotationId/comments',
+  naive_isQuotationOwnerOrAdminCommentRoute,
+  getQuotationComments,
+)
 
 // Users can only comment on their own quotation
 // Admin can comment on any quotation
-commentRouter.post('/', createComment)
+commentRouter.post('/', naive_isQuotationOwnerOrAdminCommentRoute, createComment)
 
 // Users can get info on their own comments
 // Admin can get info on any comment
-commentRouter.get('/:id', getComment)
+commentRouter.get('/:id', naive_isCommentOwnerOrAdmin, getComment)
 
 // Users can only edit their own comment
 // Admin can edit all comments
-commentRouter.put('/:id', editComment)
+commentRouter.put('/:id', naive_isCommentOwnerOrAdmin, editComment)
 
 // Users can only delete their own comment
 // Admin can delete any comments
-commentRouter.delete('/:id', deleteComment)
+commentRouter.delete('/:id', naive_isCommentOwnerOrAdmin, deleteComment)
