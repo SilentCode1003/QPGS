b2720ae71311e3737f6c8ef3719ccf45644650c6
Clueless7 <catalanjosel123@gmail.com>
2024-08-08 16:54:22 +0800

    task: update landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
+import { Button, Flex, Stack, Text } from '@mantine/core'
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <main>
-      <h1>Hello</h1>
-    </main>
+    <Flex h="100vh" justify="center" align="center" p="lg">
+      <Stack p={30}>
+        {/* <Image src="/FiveL-1.png" alt="5L Solutions logo" width={500} height={500} /> */}
+
+        <Text c="blue" size="128px" fw="bold">
+          QPGS
+        </Text>
+        <Text size="36px">Quotation and Purchase Order Generator System</Text>
+
+        <div className="mt-20">
+          <Button
+            size="xl"
+            onClick={() => {
+              router.push('/login')
+            }}
+          >
+            Go to login page
+          </Button>
+        </div>
+      </Stack>
+    </Flex>
   )
 }
